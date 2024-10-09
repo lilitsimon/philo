ef4c16faa8d510e04702f323e6b2b51756b13fc8
Lilit <lilitsimonyan94@gmail.com>
2024-10-09 15:03:03 +0200

    initial structure

---
+++ philo/philo.h
@@ -0,0 +1,24 @@
+
+#ifndef PHILO_H
+# define PHILO_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/time.h>
+
+#define MAX_PHILOS 200
+
+typedef struct s_philo
+{
+
+}   t_philo;
+
+typedef struct s_args
+{
+
+}   t_args;
+
+#endif
